680f1d2daa186039b170ca006cf2c25ac8db5da4
Holden Halferty <holdenhalferty1215@gmail.com>
2022-09-27 21:31:25 -0500

    Day 13 : Bash for looping script

---
+++ day13/LoopThis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# cerner_2tothe5th_2022
+# cerner_2^5th_2022
+#
+#   Day 13: Almost forgot todays submission! Here's a quick bash script with looping.
+#
+STR="Cerner Cares: Happy 2^5 Competition!"
+
+for COUNT in 1 2 3 4 5 ..
+do
+    echo $STR
+done
