c28094e6a0d77a34c94c5eab2777b879290795fc
Holden Halferty <holdenhalferty1215@gmail.com>
2022-09-29 23:13:55 -0500

    Day 15: Svelte Svelte Svelte
    Update BasicOutput.svelte

---
+++ day15/BasicOutput.svelte
@@ -0,0 +1,17 @@
+<!--
+    "cerner_2tothe5th_2022"
+    "cerner_2^5_2022"
+    
+    Day 15: First time with svelte. Didn't have much time today, but I will definitely be looking
+        into svelte a bit more. The link below has alot of plug and play examples that could be good
+        for some hands on learning.
+
+    Try it online here: https://svelte.dev/examples/hello-world
+-->
+<script>
+	let day = 3 * 5;
+	let str = '2^5'
+</script>
+
+<h1>Hello {str} Competitors!</h1>
+<h2>Today is Day {day} of the Competition</h2>
